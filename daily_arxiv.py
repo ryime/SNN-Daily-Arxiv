667ab9e342a6664bbbd2ddd20d94e398a3f91101
Li Lun <138178073+ryime@users.noreply.github.com>
2025-05-03 14:37:00 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -105,15 +105,17 @@ def abbr(comments):
         return comments
 
 def update_json_file(filename,data_all):
+    '''
     with open(filename,"r") as f:
         content = f.read()
         if not content:
             m = {}
         else:
             m = json.loads(content)
-            
-    json_data = m.copy() 
     
+    json_data = m.copy() 
+    '''
+    json_data = {}
     # update papers in each keywords         
     for data in data_all:
         for keyword in data.keys():
@@ -232,7 +234,7 @@ if __name__ == "__main__":
     data_collector_web= []
     
     keywords = dict()
-    keywords["Spiking Neural Network"]                 = "\"Spiking Neural Network\"OR\"Spiking Neural Networks\"OR\"Spiking Neuron\""
+    keywords["Spiking Neural Network"]                 = "\"Spiking Neural Network\"OR\"Spiking Neural Networks\"OR\"SNN\""
 
     for topic,keyword in keywords.items():
  
